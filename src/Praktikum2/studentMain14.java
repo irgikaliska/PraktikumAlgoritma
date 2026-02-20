3071d0288c62d3900d560c0c00cfdcfe766bff02
Irgi Kaliska <irgikaliska24@gmail.com>
2026-02-20 15:52:27 +0700

    Jobsheet 2

--- src/Praktikum2/studentMain14.java
+++ src/Praktikum2/studentMain14.java
@@ -13,5 +13,9 @@ public class studentMain14 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa14 mhs2 = new Mahasiswa14("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
