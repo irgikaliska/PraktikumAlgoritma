d042c9c182c372ef29462a99b26f4ea961464cdf
Irgi Kaliska <irgikaliska24@gmail.com>
2026-05-09 08:40:53 +0700

    Jobsheet 10

---
+++ src/Jobsheet11/KRSQueue.java
@@ -0,0 +1,111 @@
+package Jobsheet11;
+
+public class KRSQueue {
+    Student[] data;
+    int front, rear, size, max;
+    int approvedCount = 0;
+
+    public KRSQueue(int max) {
+        this.max = max;
+        data = new Student[max];
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+
+    boolean isEmpty() {
+        return size == 0;
+    }
+
+    boolean isFull() {
+        return size == max;
+    }
+
+    void clear() {
+        front = 0;
+        rear = -1;
+        size = 0;
+        approvedCount = 0;
+        System.out.println("Queue cleared!");
+    }
+
+    void enqueue(Student s) {
+        if (isFull()) {
+            System.out.println("Queue is full!");
+            return;
+        }
+
+        rear = (rear + 1) % max;
+        data[rear] = s;
+        size++;
+
+        System.out.println(s.name + " added to queue");
+    }
+
+    Student dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty!");
+            return null;
+        }
+
+        Student s = data[front];
+        front = (front + 1) % max;
+        size--;
+        approvedCount++;
+
+        return s;
+    }
+
+    void dequeue2() {
+        System.out.println("Processing 2 students...");
+
+        for (int i = 1; i <= 2; i++) {
+            Student s = dequeue();
+
+            if (s != null) {
+                s.print();
+            }
+        }
+    }
+
+    void printQueue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty!");
+            return;
+        }
+
+        int i = front;
+
+        while (i != rear) {
+            data[i].print();
+            i = (i + 1) % max;
+        }
+
+        data[i].print();
+    }
+
+    void peekFront2() {
+        if (size >= 1) {
+            System.out.println("First student:");
+            data[front].print();
+        }
+
+        if (size >= 2) {
+            System.out.println("Second student:");
+            data[(front + 1) % max].print();
+        }
+    }
+
+    void viewRear() {
+        if (!isEmpty()) {
+            System.out.println("Last student:");
+            data[rear].print();
+        }
+    }
+
+    void printInfo() {
+        System.out.println("Students in queue: " + size);
+        System.out.println("Students approved: " + approvedCount);
+        System.out.println("Students not approved yet: " + (30 - approvedCount));
+    }
+}
---
+++ src/Jobsheet11/KRSQueueMain.java
@@ -0,0 +1,83 @@
+package Jobsheet11;
+
+import java.util.Scanner;
+
+public class KRSQueueMain {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        KRSQueue queue = new KRSQueue(10);
+
+        int choice;
+
+        do {
+            System.out.println("\n=== KRS Approval Menu ===");
+            System.out.println("1. Add Student");
+            System.out.println("2. Process 2 Students");
+            System.out.println("3. Show Queue");
+            System.out.println("4. Show First 2 Students");
+            System.out.println("5. Show Last Student");
+            System.out.println("6. Queue Information");
+            System.out.println("7. Clear Queue");
+            System.out.println("0. Exit");
+            System.out.print("Choose: ");
+
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+
+                case 1:
+                    System.out.print("NIM: ");
+                    String nim = sc.nextLine();
+
+                    System.out.print("Name: ");
+                    String name = sc.nextLine();
+
+                    System.out.print("Study Program: ");
+                    String studyProgram = sc.nextLine();
+
+                    System.out.print("Class: ");
+                    String className = sc.nextLine();
+
+                    Student s = new Student(nim, name, studyProgram, className);
+
+                    queue.enqueue(s);
+                    break;
+
+                case 2:
+                    queue.dequeue2();
+                    break;
+
+                case 3:
+                    queue.printQueue();
+                    break;
+
+                case 4:
+                    queue.peekFront2();
+                    break;
+
+                case 5:
+                    queue.viewRear();
+                    break;
+
+                case 6:
+                    queue.printInfo();
+                    break;
+
+                case 7:
+                    queue.clear();
+                    break;
+
+                case 0:
+                    System.out.println("Program finished");
+                    break;
+
+                default:
+                    System.out.println("Invalid menu!");
+            }
+
+        } while (choice != 0);
+    }
+}
