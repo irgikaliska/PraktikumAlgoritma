d5f9d35ec9527d3f9335d3b9bef912e1fd0ab1da
Irgi Kaliska <irgikaliska24@gmail.com>
2026-05-22 13:33:56 +0700

    Jobsheet Double Linked List

---
+++ src/P13/DoubleLinkedLists.java
@@ -0,0 +1,233 @@
+package P13;
+
+public class DoubleLinkedLists{
+    Node head;
+    Node tail;
+
+    DoubleLinkedLists(){
+        head = null;
+        tail = null;
+    }
+
+    boolean isEmpty() {
+        return head == null;
+    }
+
+    void addFirst(Student data) {
+        Node newNode = new Node(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+    }
+
+    void addLast(Student data) {
+        Node newNode = new Node(data);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+    void add(int index, Student data) {
+        if (index < 0) {
+            System.out.println("Invalid index! Index cannot be negative.");
+            return;
+        }
+        if (index == 0) {
+            addFirst(data);
+            return;
+        }
+        Node temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            if (temp == null) {
+                System.out.println("Invalid index! Index out of bounds.");
+                return;
+            }
+            temp = temp.next;
+        }
+        if (temp == tail) {
+            addLast(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        newNode.next = temp.next;
+        newNode.prev = temp;
+        temp.next.prev = newNode;
+        temp.next = newNode;
+    }
+
+    void insertAfter(String key, Student data) {
+        Node newNode = new Node(data);
+        Node temp = head;
+        boolean found = false;
+        while (temp != null) {
+            if (temp.data.nim.equalsIgnoreCase(key)) {
+                found = true;
+                if (temp == tail) {
+                    addLast(data);
+                } else {
+                    newNode.next = temp.next;
+                    newNode.prev = temp;
+                    temp.next.prev = newNode;
+                    temp.next = newNode;
+
+                }
+            }
+            temp = temp.next;
+        }
+        if (!found) {
+            System.out.println("Insertion failed. Data (" + key + ") not found!!");
+        }
+    }
+
+    void print() {
+        if (!isEmpty()) {
+            Node temp = head;
+            while (temp != null) {
+                temp.data.print();
+                temp = temp.next;
+            }
+            System.out.println("");
+        } else {
+            System.out.println("Double linked list is currently empty!");
+        }
+    }
+
+    void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("Double linked list is currently empty!!");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+        }
+    }
+
+    void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Double linked list is currently empty!!");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+        }
+    }
+
+    void remove(int index) {
+        if (isEmpty()) {
+            System.out.println("Double linked list is currently empty!!");
+            return;
+        }
+        if (index < 0) {
+            System.out.println("Invalid index! Index cannot be negative.");
+            return;
+        }
+        if (index == 0) {
+            removeFirst();
+        } else {
+            Node temp = head;
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+            if (temp == tail) {
+                removeLast();
+            } else {
+                temp.prev.next = temp.next;
+                temp.next.prev = temp.prev;
+            }
+        }
+    }
+    void removeAfter(String key) {
+        if (isEmpty()) {
+            System.out.println("Double linked list is currently empty!!");
+            return;
+        }
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data.nim.equalsIgnoreCase(key)) {
+                if (temp.next == null) {
+                    System.out.println("No node after " + key + ".");
+                } else if (temp.next == tail) {
+                    removeLast();
+                } else {
+                    Node toDelete = temp.next;
+                    temp.next = toDelete.next;
+                    toDelete.next.prev = temp;
+                }
+                return;
+            }
+            temp = temp.next;
+        }
+        System.out.println("Key " + key + " not found!");
+    }
+    Student getFirst() {
+        if (isEmpty()) {
+            System.out.println("Double linked list is currently empty!!");
+            return null;
+        }
+        return head.data;
+    }
+
+    Student getLast() {
+        if (isEmpty()) {
+            System.out.println("Double linked list is currently empty!!");
+            return null;
+        }
+        return tail.data;
+    }
+
+    Student getIndex(int index) {
+        if (isEmpty()) {
+            System.out.println("Double linked list is currently empty!!");
+            return null;
+        }
+        if (index < 0) {
+            System.out.println("Invalid index! Index cannot be negative.");
+            return null;
+        }
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            if (temp == null) {
+                System.out.println("Invalid index! Index out of bounds.");
+                return null;
+            }
+            temp = temp.next;
+        }
+        if (temp == null) {
+            System.out.println("Invalid index! Index out of bounds.");
+            return null;
+        }
+        return temp.data;
+    }
+
+    int getSize() {
+        int size = 0;
+        Node temp = head;
+        while (temp != null) {
+            size++;
+            temp = temp.next;
+        }
+        return size;
+    }
+    int indexOf(String key) {
+        Node temp = head;
+        int index = 0;
+        while (temp != null) {
+            if (temp.data.nim.equalsIgnoreCase(key)) {
+                return index;
+            }
+            temp = temp.next;
+            index++;
+        }
+        System.out.println("Key " + key + " not found!");
+        return -1;
+    }
+}
